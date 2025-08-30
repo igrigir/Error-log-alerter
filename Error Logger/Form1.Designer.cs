b621281c0434c18ae6d123a59929c065f114f9d6
Igor Josić <igor-josic@hotmail.com>
2025-08-30 19:26:42 +0200

    Start projct

---
+++ Error Logger/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Error_Logger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtLogText = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.cboPath = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.chkSound = new System.Windows.Forms.CheckBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.cboInterval = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboFontSize = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.cboFontSize);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.cboInterval);
+            this.panel1.Controls.Add(this.btnStop);
+            this.panel1.Controls.Add(this.chkSound);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.cboPath);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(941, 110);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel2.Controls.Add(this.txtLogText);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 110);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(941, 501);
+            this.panel2.TabIndex = 1;
+            // 
+            // txtLogText
+            // 
+            this.txtLogText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLogText.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtLogText.Location = new System.Drawing.Point(12, 6);
+            this.txtLogText.Multiline = true;
+            this.txtLogText.Name = "txtLogText";
+            this.txtLogText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLogText.Size = new System.Drawing.Size(917, 492);
+            this.txtLogText.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(541, 10);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // cboPath
+            // 
+            this.cboPath.FormattingEnabled = true;
+            this.cboPath.Location = new System.Drawing.Point(91, 12);
+            this.cboPath.Name = "cboPath";
+            this.cboPath.Size = new System.Drawing.Size(444, 21);
+            this.cboPath.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnStart.Location = new System.Drawing.Point(622, 10);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(136, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chkSound
+            // 
+            this.chkSound.AutoSize = true;
+            this.chkSound.Location = new System.Drawing.Point(183, 53);
+            this.chkSound.Name = "chkSound";
+            this.chkSound.Size = new System.Drawing.Size(78, 17);
+            this.chkSound.TabIndex = 4;
+            this.chkSound.Text = "Play sound";
+            this.chkSound.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Coral;
+            this.btnStop.Location = new System.Drawing.Point(764, 10);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(136, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // cboInterval
+            // 
+            this.cboInterval.FormattingEnabled = true;
+            this.cboInterval.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "5",
+            "10",
+            "30",
+            "60",
+            "300"});
+            this.cboInterval.Location = new System.Drawing.Point(91, 51);
+            this.cboInterval.Name = "cboInterval";
+            this.cboInterval.Size = new System.Drawing.Size(74, 21);
+            this.cboInterval.TabIndex = 6;
+            this.cboInterval.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Interval";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(764, 44);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(136, 23);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(813, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Igor Josić";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(406, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Font size";
+            // 
+            // cboFontSize
+            // 
+            this.cboFontSize.FormattingEnabled = true;
+            this.cboFontSize.Items.AddRange(new object[] {
+            "8",
+            "10",
+            "12"});
+            this.cboFontSize.Location = new System.Drawing.Point(461, 49);
+            this.cboFontSize.Name = "cboFontSize";
+            this.cboFontSize.Size = new System.Drawing.Size(74, 21);
+            this.cboFontSize.TabIndex = 11;
+            this.cboFontSize.SelectedIndexChanged += new System.EventHandler(this.cboFontSize_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 611);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PHP Error logger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboInterval;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.CheckBox chkSound;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ComboBox cboPath;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtLogText;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cboFontSize;
+    }
+}
+
---
+++ Error Logger/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Error_Logger
+{
+    public partial class Form1 : Form
+    {
+
+        private const string ConfigFileName = "Config.ini";
+        private string previousContent = string.Empty;
+        private bool fileErrorOccurred = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPaths();
+        }
+
+
+        private void LoadPaths()
+        {
+            string configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigFileName);
+            if (File.Exists(configPath))
+            {
+                var paths = new List<string>();
+                var lines = File.ReadAllLines(configPath);
+                bool inPathsSection = false;
+
+                foreach (var line in lines)
+                {
+                    if (line.Trim().Equals("[paths]", StringComparison.OrdinalIgnoreCase))
+                    {
+                        inPathsSection = true;
+                        continue;
+                    }
+
+                    if (inPathsSection)
+                    {
+                        if (line.Trim().StartsWith("["))
+                        {
+                            break; // End of section
+                        }
+
+                        var match = Regex.Match(line, @"^(?<key>[^=]+)=(?<value>.+)$");
+                        if (match.Success)
+                        {
+                            paths.Add(match.Groups["value"].Value.Trim());
+                        }
+                    }
+                }
+
+                cboPath.DataSource = paths;
+
+                if (paths.Count > 0)
+                {
+                    cboPath.SelectedIndex = 0; // Select first path
+                }
+            }
+            else
+            {
+                MessageBox.Show("Config.ini file not found.");
+            }
+        }
+
+
+        private void SavePaths(List<string> paths)
+        {
+            string configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigFileName);
+            using (StreamWriter writer = new StreamWriter(configPath))
+            {
+                writer.WriteLine("[paths]");
+                foreach (var path in paths)
+                {
+                    writer.WriteLine($"path={path}");
+                }
+            }
+        }
+
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = Convert.ToInt32(cboInterval.Text) * 1000;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string newPath = cboPath.Text; // Pretpostavljamo da imaš TextBox za unos nove putanje
+            var paths = cboPath.DataSource as List<string>;
+            paths.Add(newPath);
+            SavePaths(paths);
+            LoadPaths(); // Reload paths after saving
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            btnStart.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            cboInterval.Text = "1";
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                btnStart.Enabled = false;
+            }else
+            {
+                btnStart.Enabled = true;
+            }
+            
+            
+            string selectedPath = cboPath.Text;
+
+            if (File.Exists(selectedPath))
+            {
+                string fileContent = File.ReadAllText(selectedPath);
+                txtLogText.Text = fileContent;
+                fileErrorOccurred = false; // Resetuj grešku
+
+                // Proveri da li je checkBox1 uključen
+                if (chkSound.Checked)
+                {
+                    // Ako je sadržaj različit, daj zvučnu poruku
+                    if (fileContent != previousContent)
+                    {
+                        Console.Beep(); // Zvučna poruka
+                        previousContent = fileContent; // Ažuriraj prethodni sadržaj
+                    }
+                }
+            }
+            else
+            {
+                if (!fileErrorOccurred) // Prikazuj grešku samo jednom
+                {
+                    MessageBox.Show("Fajl ne postoji: " + selectedPath);
+                    fileErrorOccurred = true; // Postavi grešku
+                    timer1.Enabled = false; // Stopiraj timer
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Učitaj putanju iz ComboBox
+            string selectedPath = cboPath.Text;
+
+            // Obriši sadržaj TextBox-a
+            txtLogText.Clear();
+
+            // Proveri da li fajl postoji
+            if (File.Exists(selectedPath))
+            {
+                // Isprazni sadržaj fajla
+                File.WriteAllText(selectedPath, string.Empty);
+                MessageBox.Show("Sadržaj fajla je ispraznjen: " + selectedPath);
+            }
+            else
+            {
+                MessageBox.Show("Fajl ne postoji: " + selectedPath);
+            }
+        }
+
+        private void cboFontSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Učitaj izabranu veličinu fonta iz ComboBox-a
+            if (int.TryParse(cboFontSize.SelectedItem.ToString(), out int fontSize))
+            {
+                // Postavi font veličinu za txtLogText
+                txtLogText.Font = new Font(txtLogText.Font.FontFamily, fontSize);
+            }
+        }
+    }
+}
